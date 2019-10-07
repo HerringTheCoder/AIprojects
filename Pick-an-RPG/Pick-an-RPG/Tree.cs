202150fea4c49dd64d127b20583e504a808135e9
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2019-10-08 00:50:10 +0200

    Add app sketch

---
+++ Pick-an-RPG/Pick-an-RPG/Tree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Pick_an_RPG
+{
+    public class Tree
+    {
+        public List<bool> Branch = new List<bool>();
+        public string Result()
+        {
+            int decResult=0;
+            for (int i = 0; i > Branch.Count; i++)
+            {
+                decResult = +Convert.ToInt32(Branch[i - 1]) * Convert.ToInt32(Math.Pow(2, i));
+            }
+            return FindGame(decResult);
+        }
+
+       public string FindGame(int result)
+        {
+            switch(result)
+            {
+                case 0:
+                    return "";
+                case 1:
+                  return "";
+                case 2:
+                  return "";
+                case 3:
+                    return "";
+                case 4:
+                    return "";
+                case 5:
+                    return "";
+                case 6:
+                    return "";
+                case 7:
+                    return "";
+                case 8:
+                    return "";
+                case 9:
+                    return "";
+                case 10:
+                    return "";
+                case 11:
+                    return "";
+                case 12:
+                    return "";
+                case 13:
+                    return "";
+                case 14:
+                    return "";
+                case 15:
+                    return "";
+                case 16:
+                    return "";
+                default:
+                    return "Error";
+            }
+            
+        }
+
+    }
+}
---
+++ Pick-an-RPG/Pick-an-RPG/Window1.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pick_an_RPG
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Tree MyTree;
+        public int StepCounter = 1;
+        public Window1(Tree MyTree)
+        {
+            InitializeComponent();
+            this.MyTree = MyTree;
+        }
+
+        private void LeftBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MyTree.Branch.Add(false);
+            LoadIconSet();
+            
+        }
+
+        private void RightBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MyTree.Branch.Add(true);
+            LoadIconSet();
+
+        }
+
+        private void LoadIconSet()
+        {
+            switch (StepCounter)
+            {
+                case 1:
+                    LeftImg.Source = new BitmapImage(new Uri(@"/images/action.png", UriKind.Relative));
+                    RightImg.Source = new BitmapImage(new Uri(@"/images/dice.png", UriKind.Relative));
+                    break;
+                case 2:
+                    LeftImg.Source = new BitmapImage(new Uri(@"/images/drake.png", UriKind.Relative));
+                    RightImg.Source = new BitmapImage(new Uri(@"/images/scifi.png", UriKind.Relative));
+                    break;
+                case 3:
+                    if (MyTree.Branch[2] == true) { 
+                    LeftImg.Source = new BitmapImage(new Uri(@"/images/postapo.png", UriKind.Relative));
+                        RightImg.Source = new BitmapImage(new Uri(@"/images/cosmos.png", UriKind.Relative));
+                    }
+                    else
+                    {
+                        LeftImg.Source = new BitmapImage(new Uri(@"/images/heroic.png", UriKind.Relative));
+                        RightImg.Source = new BitmapImage(new Uri(@"/images/dark.png", UriKind.Relative));
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            StepCounter++;
+        }
+    }
+}
