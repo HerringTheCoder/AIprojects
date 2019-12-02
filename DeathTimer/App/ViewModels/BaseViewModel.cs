d104ce7f418300fc22f9b8a9f3851637ad63bb92
Reszczyk <51290351+reszczyk@users.noreply.github.com>
2019-12-02 17:07:45 +0100

    Death Timer final version

---
+++ DeathTimer/App/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System;
+
+namespace DeathTimer
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+       
+
+        #region INotifyPropertyChanged
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
